c4f41e9b41197cd86571b7e2f72cba8094c77e52
akhilcharugulla <akhilcharugulla13@gmail.com>
2022-08-26 20:10:19 +0530

    added commandline runner usage

--- blogpostakhil/src/main/java/udemy/blogpost/demo/TysonApplication.java
+++ blogpostakhil/src/main/java/udemy/blogpost/demo/TysonApplication.java
@@ -1,6 +1,7 @@
 package udemy.blogpost.demo;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -15,7 +16,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EntityScan("udemy.blogpost.demo.entity")
 @ComponentScan({"udemy.blogpost.demo"})//,"udemy.blogpost.dto"
 @EnableSwagger2
-public class TysonApplication {
+public class TysonApplication implements CommandLineRunner{
+	//for swagger the url
+	//http://localhost:8081/swagger-ui.html#/
+	
 	
 	@Bean
 	public ModelMapper modelMapper() {
@@ -25,5 +29,13 @@ public class TysonApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TysonApplication.class, args);
 		System.out.println("om sairam");
+	}
+	
+	//The CommandLineRunner
+	//The run method in main main class first gets executed onstarting springboot application. 
+	//We get run method on implementing the CommlandLineRunner Interface.
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("I am in command line runner...");
 	}
 }
